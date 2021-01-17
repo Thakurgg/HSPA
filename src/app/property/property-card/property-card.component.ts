29a28ac638dc7e7de4a72274399fcd16369f5751
thakurgg <gautamithakur0@gmail.com>
2021-01-17 16:46:06 +0530

    Initial commit

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-property-card',
+    template: '<h1>I am cardn </h1>'
+}
+
+)
+export class PropertyCardComponent{
+
+}
